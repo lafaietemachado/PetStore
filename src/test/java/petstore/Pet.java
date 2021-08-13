e906c62e8bd31df0a22e3f924b423fffdd5c1b89
lafaiete.machado <lafaiete.machado@gmail.com>
2021-08-12 21:21:36 -0300

    Completei o script consultarPet (Get)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -16,21 +16,21 @@ import static org.hamcrest.Matchers.contains;
 // 3 - Classe
 public class Pet {
     // 3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; // endereÃ§o da entidade Pet
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
 
-    // 3.2 - MÃ©todos e FunÃ§Ãµes
+    // 3.2 - Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
     // Incluir - Create - Post
-    @Test // Identifica o mÃ©todo ou funÃ§Ã£o como um teste para o TestNG
+    @Test(priority = 1) // Identifica o método ou função como um teste para o TestNG // definindo a prioridade de execução
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
-        // Dado - Quando - EntÃ£o
+        // Dado - Quando - Então
         // Given - When - Then
 
         given() // Dado
@@ -39,14 +39,36 @@ public class Pet {
                 .body(jsonBody)
         .when() // Quando
                 .post(uri)
-        .then() // EntÃ£o
+        .then() // Então
                 .log().all()
                 .statusCode(200)
                 .body("name", is("Batata"))
                 .body("status", is("available"))
-                .body("category.name", is("dog"))
-                .body("tags.name", contains("sta"))
+                //.body("category.name", is("dog"))
+                .body("category.name", is("AX674RT7U0"))
+                .body("tags.name", contains("data"))
         ;
     }
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "2017070604";
 
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Batata"))
+                //.body("category.name", is("dog"))
+                .body("category.name", is("AX674RT7U0"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
+        ;
+        System.out.println("O token é " + token);
+    }
 }
